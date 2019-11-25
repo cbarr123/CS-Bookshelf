37d98fca6a3318408bdf4a26e9a10b95b1fa0d41
Carl Barringer <cbarringer1@gmail.com>
2019-11-25 08:34:47 -0600

    Initial Commit

---
+++ Models/Author.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class Author
+    {
+        [Required]
+        [MaxLength(25)]
+        [MinLength(1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(35)]
+        [MinLength(1)]
+        public string LastName { get; set; }
+
+        [MaxLength(35)]
+        [MinLength(1)]
+        public string Penname { get; set; }
+
+        [MaxLength(35)]
+        [MinLength(1)]
+        public string PreferredGenre { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
